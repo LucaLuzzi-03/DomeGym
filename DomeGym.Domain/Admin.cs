1c6754c72b4c2e9757a43630eb3c06da16b145c9
Luca Luzzi <111360538+LucaLuzzi-03@users.noreply.github.com>
2026-02-28 18:08:32 -0300

    Adding Domain models with minimum needed attributes. (#1)

---
+++ DomeGym.Domain/Admin.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DomeGym.Domain
+{
+    public class Admin
+    {
+        private readonly Guid _userId;
+        private readonly Guid _subscriptionId;
+    }
+}
---
+++ DomeGym.Domain/Subscription.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DomeGym.Domain
+{
+    public class Subscription
+    {
+        private readonly Guid _id;
+        private readonly List<Guid> _gymIds;
+    }
+}
